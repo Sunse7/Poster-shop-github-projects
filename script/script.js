94ebba8e92f97f87aa9b1cac915b8f4e877f6669
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-03-08 14:35:26 +0100

    Added menu function + add to cart function

--- script/script.js
+++ script/script.js
@@ -4,9 +4,40 @@ const dbFile = "files/posters.json";
 
 
 getJsonData();
+openMenu();
 
 function getJsonData() {
     fetch(dbFile)
     .then(data => data.json())
     .then(data => renderCards(data));
 }
+
+
+
+function openMenu() {
+    let burgerMenu = document.querySelector(".header__nav img")
+    burgerMenu.addEventListener("click", () => {
+        let liElements = document.querySelectorAll(".header__nav li:not(:last-child")
+        liElements.forEach(liEl => {
+        liEl.style.display = "block"
+        })
+        burgerMenu.style.display = "none"
+        let closeBtn = document.querySelector(".closeBtn")
+        closeBtn.addEventListener("click", () => {
+           burgerMenu.style.display = "block"
+           liElements.forEach(liEl => {
+            liEl.style.display = "none"
+            })
+        })
+    })
+}
+
+let productsInCart = []
+function addToCart(btnId, product) {
+    if (btnId === product.id) {
+        productsInCart.push(product)
+    }
+    localStorage.setItem("productsInCart", JSON.stringify(productsInCart))
+}
+
+export {addToCart}
