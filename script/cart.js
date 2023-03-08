1e09c302d1a55421c95a1acc8a4ec1430a89096b
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-03-08 15:39:48 +0100

    Render cards and sum in checkout page

---
+++ script/cart.js
@@ -0,0 +1,69 @@
+import { openMenu } from "./menu.js";
+
+//VARIABLES
+let productsInCart = JSON.parse(localStorage.getItem("productsInCart"))
+//Container for cards
+let containerEl = document.querySelector(".cards-container")
+//Container for list of items
+let cartSumEl = document.querySelector(".sum-container")
+
+openMenu()
+renderCards(productsInCart)
+
+//FUNCTIONS
+//Render Cards to UI
+function renderCards(list) {
+  list.forEach(product => {
+    let cardEl = document.createElement("section")
+    cardEl.setAttribute("class", "card")
+    cardEl.innerHTML = `
+    <img src="../images/${product.img}">
+    <div>
+      <h2>${product.name}</h2>
+    </div>
+    `
+    cardEl.appendChild(addBtn(product)) 
+    containerEl.appendChild(cardEl)
+  });
+  renderCart()
+}
+
+//Add remove button
+function addBtn(product) {
+  let btn = document.createElement("button")
+  btn.setAttribute("class", "btn")
+  btn.setAttribute("id", product.id)
+  btn.innerHTML = "Ta bort från varukorg"
+  btn.addEventListener("click", (e) => {
+    let btnId = e.target.id
+    removeFromCart(btnId, productsInCart)
+    renderCards(productsInCart)
+})
+  return btn
+}
+
+//Remove item from cart
+function removeFromCart(btnId, productsInCart) {
+  containerEl.innerHTML = ""
+  let index = productsInCart.findIndex(product => product.id === btnId)
+  productsInCart.splice(index, 1)
+  localStorage.setItem("productsInCart", JSON.stringify(productsInCart))
+  productsInCart = JSON.parse(localStorage.getItem("productsInCart"))
+}
+
+//Render list of items in cart in UI 
+function renderCart() {
+  cartSumEl.innerHTML = " "
+  productsInCart.forEach(product => {
+    let cartEl = document.createElement("section")
+    cartEl.setAttribute("class", "cart-text")
+    cartEl.innerHTML = `
+    <p>Poster ${product.name}</p>
+    <p>1</p>
+    <p>${product.price}</p>
+    `
+    cartSumEl.appendChild(cartEl)
+  })
+}
+
+
--- script/script.js
+++ script/script.js
@@ -1,38 +1,21 @@
 import { renderCards } from "./ui.js";
+import { openMenu } from "./menu.js";
 
 const dbFile = "files/posters.json";
+const productsInCart = []
 
 
 getJsonData();
 openMenu();
 
+//Fetch data
 function getJsonData() {
     fetch(dbFile)
     .then(data => data.json())
     .then(data => renderCards(data));
 }
 
-
-
-function openMenu() {
-    let burgerMenu = document.querySelector(".header__nav img")
-    burgerMenu.addEventListener("click", () => {
-        let liElements = document.querySelectorAll(".header__nav li:not(:last-child")
-        liElements.forEach(liEl => {
-        liEl.style.display = "block"
-        })
-        burgerMenu.style.display = "none"
-        let closeBtn = document.querySelector(".closeBtn")
-        closeBtn.addEventListener("click", () => {
-           burgerMenu.style.display = "block"
-           liElements.forEach(liEl => {
-            liEl.style.display = "none"
-            })
-        })
-    })
-}
-
-let productsInCart = []
+//Add item to cart
 function addToCart(btnId, product) {
     if (btnId === product.id) {
         productsInCart.push(product)
